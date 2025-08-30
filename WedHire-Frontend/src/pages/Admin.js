07dae0f78f95968121704ada0625d31d0f7993c6
007prakashmurugan-cmyk <007prakashmurugan@gmail.com>
2025-08-30 13:30:41 -0400

    Delete WedHire-Frontend directory

--- WedHire-Frontend/src/pages/Admin.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Admin() {
-  return <h1>Admin Dashboard</h1>;
-}
--- WedHire-Frontend/src/pages/Browse.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Browse() {
-  return <h1>Browse Candidates</h1>;
-}
